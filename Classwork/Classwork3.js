6c6f67f2582f3109c60e6287da442d8bdfd55e3a
Pablo Velez <pablovelezlopera3@gmail.com>
2022-10-30 19:01:10 -0500

    Classwork 3:JavaScript data types and data structures

---
+++ Classwork/Classwork3.js
@@ -0,0 +1,30 @@
+//Nuestra tarea será declarar dos variables: una con el nombre edad y la otra con el nombre peso.
+let peso;
+let edad;
+
+//Declaremos dos variables llamadas edad y peso, a las cuales debemos asignarles un valor numérico
+
+let peso1 = 34;
+let edad1 = 35;
+
+//Ahora, debemos crear una constante llamada EDAD_MINIMA y otra llamada EDAD_MAXIMA, a las cuales les asignaremos valores numéricos 18 y 99 respectivamente.
+const EDAD_MINIMA = 18;
+
+const EDAD_MAXIMA = 99;
+
+//Para este ejercicio, debemos declarar 4 variables de tipo let con los nombres: verdadero, nada, texto y numero. A cada una de ellas, tendremos que asignarle el tipo de dato que sugiera su nombre, según corresponda.
+let verdadero = true
+
+console.log(verdadero);
+
+let nada = null;
+
+console.log(nada);
+
+let texto = "Hola";
+
+console.log(texto);
+
+let numero = 15;
+
+console.log(numero);
